a0dae0a0d4f17bb15f2ab2a0da2994a11cf98282
Arpit Gupta <104790714+INTROVERTc0der@users.noreply.github.com>
2024-10-22 09:55:50 +0530

    Time: 179 ms (5.04%), Space: 206.6 MB (5.02%) - LeetHub

---
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/3566-find-the-sequence-of-strings-appeared-on-the-screen.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void solve(string ans, string target , int index , bool flag , vector<string>& answer){
+        //base case
+        if(ans.size()==target.size() && ans[ans.size()-1]==target[target.size()-1])return ;
+
+        //if flag is true
+        //need to add a new character 
+        if(flag){
+            ans+="a";
+            if(ans[index]==target[index]){
+                answer.push_back(ans);
+                solve(ans, target,index+1,flag,answer);
+            }
+            else{
+                answer.push_back(ans);
+                solve(ans, target,index,false,answer);
+            }
+        }
+        else{
+            ans[index]=ans[index]+1;
+            if(ans[index]==target[index]){
+                answer.push_back(ans);
+                solve(ans,target,index+1,true,answer);
+            }
+            else{
+                answer.push_back(ans);
+                solve(ans,target,index,flag,answer);
+            }
+        }
+
+    }
+    vector<string> stringSequence(string target) {
+        vector<string>answer;
+        solve("",target,0,true,answer);
+        return answer;
+    }
+};
