14bd9b2afa87392259ced7af8aa312062d5a40e5
Arpit Gupta <104790714+INTROVERTc0der@users.noreply.github.com>
2024-10-22 10:29:23 +0530

    Time: 25 ms (5.16%), Space: 63 MB (5.53%) - LeetHub

---
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/3566-find-the-sequence-of-strings-appeared-on-the-screen.java
@@ -0,0 +1,37 @@
+class Solution {
+    List<String> answer = new ArrayList<>();
+    
+    public void solve(String ans, String target, int index, boolean flag) {
+        // Base case
+        if (ans.length() == target.length() && ans.charAt(ans.length() - 1) == target.charAt(target.length() - 1)) 
+            return;
+        
+        // If flag is true
+        if (flag) {
+            ans += "a";
+            if (ans.charAt(index) == target.charAt(index)) {
+                answer.add(ans);
+                solve(ans, target, index + 1, flag);
+            } else {
+                answer.add(ans);
+                solve(ans, target, index, false);
+            }
+        } else {
+            // Create new string with incremented character
+            ans = ans.substring(0, index) + (char)(ans.charAt(index) + 1) + ans.substring(index + 1);
+            
+            if (ans.charAt(index) == target.charAt(index)) {
+                answer.add(ans);
+                solve(ans, target, index + 1, true);
+            } else {
+                answer.add(ans);
+                solve(ans, target, index, flag);
+            }
+        }
+    }
+    
+    public List<String> stringSequence(String target) {
+        solve("", target, 0, true);
+        return answer;
+    }
+}
