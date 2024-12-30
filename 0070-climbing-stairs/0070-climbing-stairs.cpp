76bfada4e67bbb7b00e998aec6be54b5625d59d5
Arpit Gupta <104790714+INTROVERTc0der@users.noreply.github.com>
2024-12-31 00:53:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,17 +7,21 @@ public:
         int fn = solveM(n-1,dp)+solveM(n-2,dp);
         return dp[n]=fn;
     }
-    // int solveT(int n){
-    //     vector<int>dp(n+1,-1);
-    //     dp[1]=1;
-    //     dp[2]=2;
-    //     for(int i=3;i<=n;i++){
-
-    //     }
-
-    // }
+    int solveT(int n){
+        if(n==1)return 1;
+        if(n==2)return 2;
+        vector<int>dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            int fn = dp[i-1]+dp[i-2];
+            dp[i]=fn;
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solveM(n,dp);
+        return solveT(n);
+        //vector<int>dp(n+1,-1);
+        //return solveM(n,dp);
     }
 };
